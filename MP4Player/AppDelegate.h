f439d2826b1103c31a8535c93cd5e78a1e3d519c
pengpengduduguagua <2979468570@qq.com>
2019-06-24 20:10:08 +0800

    Initial Commit

---
+++ MP4Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MP4Player
+//
+//  Created by admin on 2019/6/24.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MP4Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MP4Player
+//
+//  Created by admin on 2019/6/24.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
